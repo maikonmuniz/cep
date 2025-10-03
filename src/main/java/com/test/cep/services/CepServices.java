bddbf5865225b85ee0dbfe0212526413a91affa7
maikon.muniz <maikon.muniz@yahoo.com.br>
2025-10-03 11:23:47 -0300

    feat: create service and if field cep is empty

--- src/main/java/com/test/cep/services/CepServices.java
+++ src/main/java/com/test/cep/services/CepServices.java
@@ -1,4 +1,22 @@
 package com.test.cep.services;
 
+import com.test.cep.infra.exception.ExceptionAdapter;
+import com.test.cep.infra.http.ApiAdapter;
+
 public class CepServices {
+
+    private final ApiAdapter apiAdapter;
+    private final ExceptionAdapter exceptionAdapter;
+
+    public CepServices (ApiAdapter apiAdapter, ExceptionAdapter exceptionAdapter) {
+        this.apiAdapter = apiAdapter;
+        this.exceptionAdapter = exceptionAdapter;
+    }
+
+    public Object execute (String cep) {
+        if (cep == null || cep.isEmpty()) {
+            throw this.exceptionAdapter.badRequest("Campo cep está em branco!");
+        }
+        return this.apiAdapter.Get(cep);
+    }
 }
