dc5ce16b5ef2767ed0f6027d0c6767c2ed368444
maikon.muniz <maikon.muniz@yahoo.com.br>
2025-10-03 11:22:36 -0300

    feat: create main service cep

--- src/main/java/com/test/cep/main/CepMain.java
+++ src/main/java/com/test/cep/main/CepMain.java
@@ -1,4 +1,22 @@
 package com.test.cep.main;
 
-public class MainCep {
+import com.test.cep.infra.exception.ExceptionAdapter;
+import com.test.cep.infra.http.ApiAdapter;
+import com.test.cep.services.CepServices;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class CepMain {
+
+    @Bean
+    ApiAdapter createApiAdapter (RestTemplate api) {
+        return new ApiAdapter(api);
+    }
+
+    @Bean
+    CepServices createCepServices (ApiAdapter apiAdapter, ExceptionAdapter exceptionAdapter) {
+        return new CepServices(apiAdapter, exceptionAdapter);
+    }
 }
